8c2a7d0f6006a03ac34c752f7b93f7da7f19ac50
Jose Angel <jafb19@outlook.com>
2021-09-23 22:02:15 +0200

    TodoPolvo Terminado

--- Pelicula/js/pelicula.js
+++ Pelicula/js/pelicula.js
@@ -10,22 +10,27 @@ class Pelicula {
     this.titulo = 'Tiroteo en Mississippi'
     this.pueblo = new Pueblo('TodoPolvo', 'muy polvoriento, en mitad del desierto de arizona y a donde nadie queria llegar')
     this.narrador = new Narrador()
-    this.paco = new PersonajeBueno('Paco')
-    this.maria = new PersonajeBueno('Maria')
-    this.morgan = new PersonajeMalo('Morgan Ojo morao\'')
+    this.paco = new PersonajeBueno('Paco ')
+    this.maria = new PersonajeBueno('Maria ')
+    this.morgan = new PersonajeMalo('Morgan Ojo morao \'')
 
     this.iniciar()
   }
   iniciar(){
     document.write(`<h1>${this.titulo}</h1>`)
     document.write(`<p>${this.pueblo.nombre} era un pueblo ${this.pueblo.descripcion}</p>`)
-    this.paco.hablar(`Hola ${this.maria.nombre}. Hoy hace un dia esplendido.`)
-    this.maria.hablar(`Hola ${this.paco.nombre}, la verdad es que si`)
-    this.narrador.hablar('Ambos se miraron un instante y siguieron su camino...')
-    this.morgan.hablar('Vaya pueblo más... polvoriento')
-    this.morgan.hablar('¡Eh tú! ¡Pringao! Dame tu caballo y la cartera')
+    this.paco.hablar(`: Hola ${this.maria.nombre}. Hoy hace un dia esplendido.`)
+    this.maria.hablar(`: Hola ${this.paco.nombre}, la verdad es que si`)
+    this.narrador.hablar(' Ambos se miraron un instante y siguieron su camino...')
+    this.morgan.hablar(': Vaya pueblo más... polvoriento')
+    this.paco.hablar(': Hablaras por ti...')
+    this.morgan.hablar(': ¡Eh tú! ¡Pringao! Sabes con quien estas hablando?')
+    this.maria.hablar(': Con un... Polvoriento?')
+    this.narrador.hablar(' Mientras Paco se destornillaba, Morgan tenia cara de pocos amigos')
+    this.narrador.hablar(' La tension se palpaba en el ambiente...')
+    this.morgan.hablar(' Diselo a mi revolver!')
     
-    let tiroteoRandom=Math.floor(Math.random() * 10+1)
+    let tiroteoRandom=Math.floor(Math.random() * 4+2)
       while(tiroteoRandom>0){
        
         this.paco.arma.disparar()
@@ -34,8 +39,10 @@ class Pelicula {
         
         tiroteoRandom--
     }
-    
+    this.narrador.hablar('Despues de un intenso tiroteo y malheridos, no quedaba nada por hacer ')
+    this.narrador.hablar(' El lejano pueblo de TodoPolvo se rendia bajo las garras de Morgan..')
   }
+  
 
 }
 
@@ -59,7 +66,11 @@ class Personaje {
 
     this.nombre = nombre
     this.arma = new Arma()
-    
+    this.enemigos=[]
+
+  }
+  addEnemigos(personaje){
+    this.enemigos.push(personaje)
   }
 
   hablar(texto){
@@ -69,7 +80,7 @@ class Personaje {
 
 class PersonajeMalo extends Personaje {
   hablar(texto){
-    document.write(`<p class=malo><span>${this.nombre}:</span>GRRRR${texto}</p>`)
+    document.write(`<p class=malo><span>${this.nombre}</span>GRRRR ${texto}</p>`)
   }
 }
 
@@ -97,8 +108,7 @@ class Arma {
 
     }else{
       document.write('<p>¡¡ CLICK !! </p>')
-      document.write('<p>¡¡ RECARGANDO !! </p>')
-      this.balas=1
+      this.balas++
     }
   }
 }
